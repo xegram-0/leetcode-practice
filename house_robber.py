3f86b4a7937cce7312b7825eb48652bbe95b3daa
xerox <nguyenhongnhan9a5@gmail.com>
2025-01-26 16:40:58 +0700

    testing

--- house_robber.py
+++ house_robber.py
@@ -11,9 +11,19 @@ def houseRob(givenHouse:list) -> str:
     
     #print(houseRobbed)
     return f"The max profit is {robHouse2}."
+
+def houseRob2(givenHouse:list) -> str:
+    robHouse1:int = givenHouse[0]
+    robHouse2:int = max(givenHouse[0],givenHouse[1])
+    for i in range(len(givenHouse)):
+        robHouse2 = max(robHouse1[i-2] + givenHouse[i], robHouse2[i-1])
+
+
+    return f"{robHouse2}"
 def main():
-    givenHouse:list = [1,9,5,7,13,6]
-    print(houseRob(givenHouse))
+    givenHouse:list = [2,7,3,1,4,2,1,8]
+    #print(houseRob(givenHouse))
+    print(houseRob2(givenHouse))
 
 if __name__ == "__main__":
     main()
