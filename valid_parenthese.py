e0aff7d5b0c3cb7a8f1b109542f65b32fb11e93f
xerox <nguyenhongnhan9a5@gmail.com>
2025-02-03 17:01:57 +0700

    Added comments

--- valid_parenthese.py
+++ valid_parenthese.py
@@ -1,16 +1,16 @@
 def checkValid(theString:str):
     stack:list = []
     parentheseHashMap:dict = {')':'(',']':'[','}':'['}
-
+    # start with closing parenthese and pop at the most behind in the list
     for char in theString:
         if char in parentheseHashMap:
-            if stack and stack[-1] == parentheseHashMap[char]:
+            if stack and stack[-1] == parentheseHashMap[char]: # the check of matching
                 stack.pop()
             else:
                 return False
         else:
-            stack.append(char)
-    return True if not stack else False
+            stack.append(char) # if the stuff is the opening parenthese
+    return True if not stack else False # stack has to be empty
 def main():
     s1:str = "()"
     s2:str = "()[]"
